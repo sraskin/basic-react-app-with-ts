7b6ad7b24ca2e60d719e8085b2d7cd40a194b435
Shariar Khan <shuvho.91@gmail.com>
2024-02-09 21:42:24 +0600

    Created some components, used states, used props, user dynamic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import Message from "./Message";
+import ListGroup from './components/ListGroup'
+import Button from './components/Button'
+import Alert from './components/Alert'
+import { useState } from 'react'
 
 function App() {
-    return <div><Message/></div>;
+   const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
+   const handleSelectItem = (item: string) => {
+      console.log(item)
+   }
+   const [alertVisible, setAlertVisibility] = useState(false)
+   return (
+      <div>
+         {alertVisible && (
+            <Alert onClose={() => setAlertVisibility(false)}>
+               This is a primary alert—check it out!
+            </Alert>
+         )}
+         <ListGroup
+            items={items}
+            headings='List of Items'
+            onSelectItem={handleSelectItem}
+         />
+         <Button onClick={() => setAlertVisibility(true)}>Click me</Button>
+      </div>
+   )
 }
 
-export default App;
+export default App
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+interface Props {
+   items: string[]
+   headings: string
+   onSelectItem: (item: string) => void
+}
+
+function ListGroup({ items, headings, onSelectItem }: Props) {
+   const [selectedIndex, setSelectedIndex] = useState(-1)
+   return (
+      <>
+         <h1>{headings}</h1>
+         {items.length === 0 && <p>No items found</p>}
+         <ul className='list-group'>
+            {items.map((item, index) => (
+               <li
+                  className={
+                     selectedIndex === index
+                        ? 'list-group-item active'
+                        : 'list-group-item'
+                  }
+                  key={item}
+                  onClick={() => {
+                     setSelectedIndex(index)
+                     onSelectItem(item)
+                  }}
+               >
+                  {item}
+               </li>
+            ))}
+         </ul>
+      </>
+   )
+}
+
+export default ListGroup
